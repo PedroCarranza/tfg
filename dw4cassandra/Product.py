e3ece340137ddf61b3e994e84b3d8e6f71725f20
Pedro Carranza <pdbcarranza@gmail.com>
2021-05-20 12:09:23 -0300

    Week changes

--- dw4cassandra/Product.py
+++ dw4cassandra/Product.py
@@ -1,14 +1,19 @@
 from cassandra.cqlengine.models import Model
 from cassandra.cqlengine import columns
-
+from cassandra.cqlengine.management import sync_table
 
 class Product(Model):
+    __keyspace__ = 'olist'
+    __table_name__ = 'product'
+    __connection__ = 'cluster1'
     id = columns.Text(primary_key=True)
-    category_name = columns.Text()
-    nameLenght = columns.Integer()
-    descriptionLenght = columns.Integer()
+    categoryName = columns.Text()
+    nameLength = columns.Integer()
+    descriptionLength = columns.Integer()
     photosTotal = columns.Integer()
     weightGrams = columns.Integer()
     lengthCm = columns.Integer()
     heightCm = columns.Integer()
     widthCm = columns.Integer()
+
+sync_table(Product)
--- dw4cassandra/insert.py
+++ dw4cassandra/insert.py
@@ -1,18 +1,26 @@
 import csv
 from dw4cassandra.Product import Product
 from cassandra.cluster import Cluster
+from cassandra.cqlengine import connection
 
-with open('csv/olist_products_dataset.csv', newline='') as csvProd:
+with open('../csv/olist_products_dataset.csv') as csvProd:
     readerProd = csv.reader(csvProd, delimiter=',', quotechar='"')
 
-    cluster = Cluster()
-    session = cluster.connect("dw4cassandra")
+    #cluster = Cluster()
+    #session = cluster.connect()
 
+    connection.register_connection('cluster1', ['127.0.0.1'])
+
+    readerProd.next()
     for row in readerProd:
-        Product.create(id=row[0], nameLength=int(row[1]),
-                       descriptionLenght=int(row[2]),
-                       photosTotal=int(row[3]),
-                       weightGrams=int(row[4]),
-                       lengthCm=int(row[5]),
-                       heightCm=int(row[6]),
-                       widthCm=int(row[7]))
+        product = Product(id=(row[0]),
+                       categoryName=row[1],
+                       nameLength=int(row[2]),
+                       descriptionLength=int(row[3]),
+                       photosTotal=int(row[4]),
+                       weightGrams=int(row[5]),
+                       lengthCm=int(row[6]),
+                       heightCm=int(row[7]),
+                       widthCm=int(row[8]))
+        product.save()
+        print(row[0])
