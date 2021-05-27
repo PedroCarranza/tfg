40e19c16d4938c68b936f140d4371b29fc0c6792
Pedro Carranza <pdbcarranza@gmail.com>
2021-05-27 11:46:34 -0300

    Weekly Update

--- dw4cassandra/insert.py
+++ dw4cassandra/insert.py
@@ -1,26 +1,27 @@
 import csv
-from dw4cassandra.Product import Product
+import os
 from cassandra.cluster import Cluster
 from cassandra.cqlengine import connection
+
+if os.getenv('CQLENG_ALLOW_SCHEMA_MANAGEMENT') is None:
+    os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = '1'
+
+connection.register_connection('cluster1', ['127.0.0.1'])
+
+from dw4cassandra.Product import Product
 
 with open('../csv/olist_products_dataset.csv') as csvProd:
     readerProd = csv.reader(csvProd, delimiter=',', quotechar='"')
 
-    #cluster = Cluster()
-    #session = cluster.connect()
-
-    connection.register_connection('cluster1', ['127.0.0.1'])
-
     readerProd.next()
     for row in readerProd:
-        product = Product(id=(row[0]),
+        product = Product(id=long(row[0]),
                        categoryName=row[1],
-                       nameLength=int(row[2]),
-                       descriptionLength=int(row[3]),
-                       photosTotal=int(row[4]),
-                       weightGrams=int(row[5]),
-                       lengthCm=int(row[6]),
-                       heightCm=int(row[7]),
-                       widthCm=int(row[8]))
+                       nameLength=(int(row[2]) if row[2] != '' else None),
+                       descriptionLength=(int(row[3]) if row[3] != '' else None),
+                       photosTotal=(int(row[4]) if row[4] != '' else None),
+                       weightGrams=(int(row[5]) if row[5] != '' else None),
+                       lengthCm=(int(row[6]) if row[6] != '' else None),
+                       heightCm=(int(row[7]) if row[7] != '' else None),
+                       widthCm=(int(row[8]) if row[8] != '' else None))
         product.save()
-        print(row[0])
