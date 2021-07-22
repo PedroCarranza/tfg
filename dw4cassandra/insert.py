d730fb0570bf272485d735b3fde682b4a6d50051
Pedro Carranza <pdbcarranza@gmail.com>
2021-07-22 10:37:46 -0300

    Weekly Update 22/07

--- dw4cassandra/insert.py
+++ dw4cassandra/insert.py
@@ -18,6 +18,7 @@ from Review import Review
 from OrderSales import OrderSales
 from Payment import Payment
 
+"""
 with open('../csv/olist_products_dataset.csv') as csvProd:
     readerProd = csv.reader(csvProd, delimiter=',', quotechar='"')
 
@@ -95,12 +96,14 @@ with open('../csv/olist_order_items_dataset.csv') as csvItens:
         sale.save()
 
 print("Acabei de inserir no sale")
+"""
 
 with open('../csv/olist_orders_dataset.csv') as csvOrd:
     readerOder = csv.reader(csvOrd, delimiter=',', quotechar='"')
 
     readerOder.next()
 
+    cont = 0
     for row in readerOder:
         purchase = datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S") if row[3] != '' else None
         approved_at = datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S") if row[4] != '' else None
@@ -112,9 +115,15 @@ with open('../csv/olist_orders_dataset.csv') as csvOrd:
 
         for sale in sales:
             sale.customer_id = int(row[1], 16)
-            sale.hours_to_approval = int((approved_at - purchase).total_seconds() / 3600)
-            sale.hours_at_seller = int((delivered_carrier - approved_at).total_seconds() / 3600)
-            sale.hours_at_carrier = int((estimated_delivery - delivered_carrier).total_seconds() / 3600)
+            if (purchase is not None and approved_at is not None and delivered_carrier is not None and
+                    delivered_customer is not None and estimated_delivery is not None):
+                sale.hours_to_approval = int((approved_at - purchase).total_seconds() / 3600)
+                sale.hours_at_seller = int((delivered_carrier - approved_at).total_seconds() / 3600)
+                sale.hours_at_carrier = int((estimated_delivery - delivered_carrier).total_seconds() / 3600)
+            else:
+                sale.hours_to_approval = None
+                sale.hours_at_seller = None
+                sale.hours_at_carrier = None
             sale.update()
 
         #if len(sales) > 0:
@@ -128,6 +137,9 @@ with open('../csv/olist_orders_dataset.csv') as csvOrd:
                         delivered_customer=delivered_customer,
                         estimated_delivery=estimated_delivery)
         orderSales.save()
+        cont += 1
+        if cont % 1000 == 0:
+            print('Contador: ', cont)
 
 print("Acabei de inserir no OrderSale e alterar o Sale")
 
